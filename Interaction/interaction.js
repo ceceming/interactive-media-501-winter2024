d0ab50c2c8685be947fc4ed9ac938a4b5500b64e
ceceming <150284869+ceceming@users.noreply.github.com>
2024-03-23 17:06:15 -0400

    edit p3

--- Interaction/interaction.js
+++ Interaction/interaction.js
@@ -1,9 +1,14 @@
-fetch('http://worldtimeapi.org/api/timezone/America/Toronto')
-    .then(response => response.json())
-    .then(data => {
-        const torontoTime = new Date(data.datetime);
-        document.getElementById('torontoTime').innerText = torontoTime.toLocaleString();
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });  
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
+
+setInterval(changeBackgroundColor, 5000); // Change color every 5 seconds
