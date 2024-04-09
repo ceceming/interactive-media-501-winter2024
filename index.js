2f840b997ef6b1401a285ff9bb4020c36f6a882f
ceceming <150284869+ceceming@users.noreply.github.com>
2024-04-09 22:16:00 +0300

    index improvements continue

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const popupBtn = document.getElementById('popup-btn');
-const popupMenu = document.getElementById('popup-menu');
+const menuContainers = document.querySelectorAll('.menu-container');
 
-popupBtn.addEventListener('click', function() {
-  popupMenu.classList.toggle('show');
-});
+menuContainers.forEach((container) => {
+  const btn = container.querySelector('.popup-btn');
+  const menu = container.querySelector('.popup-menu');
 
-// Close the menu when clicking outside
-document.addEventListener('click', function(event) {
-  if (!popupMenu.contains(event.target) && !popupBtn.contains(event.target)) {
-    popupMenu.classList.remove('show');
-  }
+  btn.addEventListener('click', function() {
+    menu.classList.toggle('show');
+  });
+
+  // Close the menu when clicking outside
+  document.addEventListener('click', function(event) {
+    if (!menu.contains(event.target) && !btn.contains(event.target)) {
+      menu.classList.remove('show');
+    }
+  });
 });
