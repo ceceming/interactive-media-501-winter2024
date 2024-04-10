e0d601b3cc610e267fd1fcd0b72aa9909bc24642
ceceming <150284869+ceceming@users.noreply.github.com>
2024-04-10 08:45:50 +0300

    index updates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const menuContainers = document.querySelectorAll('.menu-container');
 menuContainers.forEach((container) => {
   const btn = container.querySelector('.popup-btn');
   const menu = container.querySelector('.popup-menu');
+  const menu1 = container.querySelector('.popup-menu1');
 
   btn.addEventListener('click', function() {
     menu.classList.toggle('show');
@@ -12,6 +13,17 @@ menuContainers.forEach((container) => {
   document.addEventListener('click', function(event) {
     if (!menu.contains(event.target) && !btn.contains(event.target)) {
       menu.classList.remove('show');
+    }
+  });
+
+  btn.addEventListener('click', function() {
+    menu1.classList.toggle('show');
+  });
+
+  // Close the menu when clicking outside
+  document.addEventListener('click', function(event) {
+    if (!menu1.contains(event.target) && !btn.contains(event.target)) {
+      menu1.classList.remove('show');
     }
   });
 });
